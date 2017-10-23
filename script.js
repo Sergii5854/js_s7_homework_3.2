cf53b9acc16dc4628dd4dc02607ea958484aea65
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-23 11:57:16 +0300

    add help to js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 // ///////////////////////////
 // v1
 // ///////////////////////////
- console.info("v1.6");
+ console.info("v1.1");
 
- 
+ // beginning
+function Tamagochi(firstName, lastName) {
+		this.firstName = firstName || 'First Name';
+		this.lastName = lastName || 'Second Name';
+		var shoutTimes = 0;
+
+    this.shout = function(word) {
+        shoutTimes++;
+        console.log(this.firstName + ' shouted: "' + word + '"');
+    };
+
+    this.getShoutTimes = function() {
+        return shoutTimes;
+    };
+}
+
+var Vasya = new Tamagochi('Vasya', 'Pupkin');
+Vasya.shout('Ky');
+Vasya.shout('Ka');
+Vasya.shout('Ri');
+Vasya.shout('Ky');
+console.log('Vasya.getShoutTimes', Vasya.getShoutTimes());
+
+var Petya = new Tamagochi('Petya', 'Peshkin');
+console.log('Petya.getShoutTimes', Petya.getShoutTimes());
