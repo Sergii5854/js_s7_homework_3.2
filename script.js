ac63054ca5ca1ccf3de678e3668d4b5294dc68e4
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-25 21:24:36 +0300

    add sleep

--- script.js
+++ script.js
@@ -27,20 +27,27 @@ function Tamagochi(firstName, lastName) {
 
     this.go_to_gym = function(){
     	return (
-    		power+=3,
-				speed+=1,
-				feed -=2,
-				console.log('Now ' + this.firstName + ' your power is : "' + power + '"'  + ' your speed is : "' + speed + '"' + ' but your feed is : "' + feed + '"')
-    		)
+    		power += 3,
+			speed += 1,
+			feed  -= 2,
+			console.log('Now ' + this.firstName + ' your power is : "' + power + '"'  + ' your speed is : "' + speed + '"' + ' but your feed is : "' + feed + '"')
+		)
     }
 
     this.go_to_lanch = function(){
     	return (
-    		power-=2,
-				speed-=2,
-				feed +=2,
-				console.log('Now ' + this.firstName + ' your feed is : "' + feed + '"'  + ' but your speed  and power decrease for 2 point and your speed is : "' + speed + '"' + ' , power : "' + power + '"')
-    		)
+    		power -= 2,
+    		speed -= 2,
+    		feed += 2,
+    		console.log('Now ' + this.firstName + ' your feed is : "' + feed + '"'  + ' but your speed  and power decrease for 2 point and your speed is : "' + speed + '"' + ' , power : "' + power + '"')
+    	)
+    }
+
+        this.go_to_sleap = function(){
+        return (
+            sleap = true,
+            console.log("Z - z - z - Zzz")
+        )
     }
 
     var help = function(){
@@ -48,7 +55,7 @@ function Tamagochi(firstName, lastName) {
     	console.log('.help()')
     	console.log('.go_to_gym() ')
     	console.log('.go_to_lanch()' )
-    	console.log()
+    	console.log('.go_to_sleap()')
     	console.log()
     	console.log()
     	console.log()
